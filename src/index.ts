9454e8bc80dd1c6f85447f338345066d31e0d978
mennaayman94 <mennaayman94@gmail.com>
2024-07-17 22:52:16 +0300

    feat: add rate limiter ,socket io and more dependecies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as https from 'https';
 import * as fs from 'fs';
 import * as path from"path"
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import logger from './middlewares/logger';
 import morgan from 'morgan';
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import limiter from './utils/rateLimiter';
 dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
@@ -13,8 +16,10 @@ const port = process.env.PORT|| 8000;  // HTTPS default port
 // Middleware
 app.use(bodyParser.json());
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-
+app.use(helmet())
+app.use(cookieParser())
 app.use(morgan('combined', { stream: accessLogStream }));
+app.use(logger)
 // Example route
 app.get('/',(req: Request, res: Response) => {
   res.send('Hello, HTTPS with Express!');
@@ -29,4 +34,12 @@ const options = {
 // Create HTTPS server with Express
 https.createServer(options, app).listen(port, () => {
   console.log(`Server is running on https://localhost:${port}`);
+});
+
+app.use((error:any, req:Request, res:Response, next:NextFunction) => {
+  if (error.name === 'ValidationError') {
+    res.status(400).json({ error: error.message });
+} else {
+    res.status(500).send('Internal Server Error');
+}
 });
